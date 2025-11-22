2cb297e83ef0f1de53cdb5b38fd38f62896a28f5
Ly Meng <mengsaoly@gmail.com>
2025-11-22 21:56:24 +0700

    OOP-Blueprint

---
+++ src/User.java
@@ -0,0 +1,12 @@
+public class User {
+//    static ==> name aeg
+    public int age = 10;
+    public String name = "John";
+
+//    Behavior ==> Bark Eat  Sleep
+
+    public void Sleep() {
+        System.out.println("Lymeng");
+    }
+
+}

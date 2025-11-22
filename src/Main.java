390ce223ef9489baa32a3a6aeac5ee3084d804c3
Ly Meng <mengsaoly@gmail.com>
2025-11-22 22:20:26 +0700

    OOP-Used -(Modifier)

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//        User user1 = new User();
+//
+//        user1.Sleep();
+//
+//        System.out.println("Age:"+user1.age);
+//        System.out.println("Name:"+user1.name);
         User user1 = new User();
-
-        user1.Sleep();
-
-        System.out.println("Age:"+user1.age);
-        System.out.println("Name:"+user1.name);
+        User user2 = new User();
+        user1.Register("Lymeng","Meng@gamil.com",20,"Male","123");
+        user2.Register("Lymeng","Meng@gamil.com",20,"Male","123");
+        user1.Display();
+        System.out.println("====================");
+        user2.Display();
 
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,12 +1,33 @@
 public class User {
 //    static ==> name aeg
-    public int age = 10;
-    public String name = "John";
+//    public int age = 10;
+//    public String name = "John";
+//
+//   Behavior ==> Bark Eat  Sleep
+//
+//    public void Sleep() {
+//        System.out.println("Lymeng");
+//    }
+    public String name;
+    private String Email;
+    protected int Age;
+    String Gender;
+    String Password;
 
-//    Behavior ==> Bark Eat  Sleep
-
-    public void Sleep() {
-        System.out.println("Lymeng");
+    public void Register(String Username, String email, int age, String gender, String password) {
+        name = Username;
+        Email = email;
+        Age = age;
+        Gender = gender;
+        Password = password;
     }
+    public void Display() {
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + Email);
+        System.out.println("Age: " + Age);
+        System.out.println("Gender: " + Gender);
+        System.out.println("Password: " + Password);
+    }
+
 
 }

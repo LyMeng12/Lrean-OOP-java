167df3d03505cd9927642443eb5e6f128297d46c
Ly Meng <mengsaoly@gmail.com>
2025-11-23 00:16:17 +0700

    OOP-Class and Object

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,33 @@ public class Main {
 //
 //        System.out.println("Age:"+user1.age);
 //        System.out.println("Name:"+user1.name);
-        User user1 = new User();
-        User user2 = new User();
-        user1.Register("Lymeng","Meng@gamil.com",20,"Male","123");
-        user2.Register("Lymeng","Meng@gamil.com",20,"Male","123");
-        user1.Display();
-        System.out.println("====================");
-        user2.Display();
+//        User user1 = new User();
+//        User user2 = new User();
+//        user1.Register("Lymeng","Meng@gamil.com",20,"Male","123");
+//        user2.Register("Lymeng","Meng@gamil.com",20,"Male","123");
+//        user1.Display();
+//        System.out.println("====================");
+//        user2.Display();
+
+        User emp = new User(1,"Meng","Ly",200);
+        System.out.println(emp.toString());
+        System.out.println("ID: "+emp.getId());
+        System.out.println("First-Name: "+emp.getFirstname());
+        System.out.println("Last-Name: "+emp.getLastname());
+        System.out.println("Full-Name: "+emp.getName());
+        System.out.println("Salary: "+emp.getSalary()+"$");
+        System.out.println("Annual: "+emp.getAnnualSalary()+"$");
+        System.out.println("Raise Salary: "+emp.raiseSalary(10)+"$");
+        System.out.println("=============================================");
+        User emp1 = new User(1,"Sa","Ly",1000);
+        System.out.println(emp1.toString());
+        System.out.println("ID: "+emp1.getId());
+        System.out.println("First-Name: "+emp1.getFirstname());
+        System.out.println("Last-Name: "+emp1.getLastname());
+        System.out.println("Full-Name: "+emp1.getName());
+        System.out.println("Salary: "+emp1.getSalary()+"$");
+        System.out.println("Annual: "+emp1.getAnnualSalary()+"$");
+        System.out.println("Raise Salary: "+emp1.raiseSalary(10)+"$");
 
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,81 @@
 public class User {
+//    fiel/variable
+    private int Id;
+    private String Lastname;
+    private String Firstname;
+    private int salary;
+//    method
+//    constructor
+    public User(int id, String lastname , String firstname , int salary){
+        this.Id = id;
+        this.Lastname = lastname;
+        this.Firstname = firstname;
+        this.salary = salary;
+    }
+    public int getId() {
+        return this.Id;
+    }
+    public String getLastname() {
+        return this.Lastname;
+    }
+    public String getFirstname() {
+        return this.Firstname;
+    }
+    public String getName() {
+        return this.Firstname+" "+this.Lastname;
+    }
+    public int getSalary() {
+        return this.salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public int getAnnualSalary(){
+        return this.salary * 12;
+    }
+    public int raiseSalary(int percent){
+        return (salary*percent)/100 + salary;
+    }
+    public String toString(){
+        return "Employees [id= "+this.Id +" , name=" +this.getName() +" , salary= "+this.getSalary() +"]";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    static ==> name aeg
 //    public int age = 10;
 //    public String name = "John";
@@ -8,26 +85,28 @@ public class User {
 //    public void Sleep() {
 //        System.out.println("Lymeng");
 //    }
-    public String name;
-    private String Email;
-    protected int Age;
-    String Gender;
-    String Password;
+//    public String name;
+//    private String Email;
+//    protected int Age;
+//    String Gender;
+//    String Password;
+//
+//    public void Register(String Username, String email, int age, String gender, String password) {
+//        name = Username;
+//        Email = email;
+//        Age = age;
+//        Gender = gender;
+//        Password = password;
+//    }
+//    public void Display() {
+//        System.out.println("Name: " + name);
+//        System.out.println("Email: " + Email);
+//        System.out.println("Age: " + Age);
+//        System.out.println("Gender: " + Gender);
+//        System.out.println("Password: " + Password);
+//    }
 
-    public void Register(String Username, String email, int age, String gender, String password) {
-        name = Username;
-        Email = email;
-        Age = age;
-        Gender = gender;
-        Password = password;
-    }
-    public void Display() {
-        System.out.println("Name: " + name);
-        System.out.println("Email: " + Email);
-        System.out.println("Age: " + Age);
-        System.out.println("Gender: " + Gender);
-        System.out.println("Password: " + Password);
-    }
+
 
 
 }

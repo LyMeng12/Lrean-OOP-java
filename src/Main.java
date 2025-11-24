612dac44cdb5605d54d85695141202904eaf7647
Ly Meng <mengsaoly@gmail.com>
2025-11-24 14:21:03 +0700

    OOP-constructor and Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
 import java.time.LocalDate; // import the LocalDate class
+import java.util.Scanner;
 
 public class Main {
-    String Brand = "Toyota";
-    int year = 2020;
-
-    public static void driv(){
-        System.out.println("car is drive!");
+    private String name ;
+    private int age ;
+    public Main(String name, int age) {
+        this.name = name;
+        this.age = age ;
     }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+
     public static void main(String[] args) {
-        Main car = new Main();
-        System.out.println(car.Brand);
-        System.out.println(car.year);
-        car.driv();
+        Main student = new Main("Meng",12);
+        student.setAge(20);
+        student.setName("Meng");
+        System.out.println(student.getName());
+        System.out.println(student.getAge());
+        System.out.println(student.name);
+        System.out.println(student.age);
     }
 }

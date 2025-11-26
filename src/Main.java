f8f58302eacc26d3d8ac72ab81ff527011b1d8ae
Ly Meng <mengsaoly@gmail.com>
2025-11-26 15:32:29 +0700

    OOP-abstraction_array

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Employees sa = new Manager("meng",2000,200);
-        System.out.println(sa.name);
-        System.out.println(sa.getTotalSalary());
+        Vehicle[] sar = {new Car(), new Motorcycle()};
+        for (Vehicle v : sar ){
+            v.start();
+        }
     }
 }

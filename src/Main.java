6d495e1be445d18bcfdb20b48d3d5bbb353fb179
LyMeng12 <mengsaoly@gmail.com>
2025-12-07 22:11:42 +0700

    OOP

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
-    private String name;
-    private int age ;
-    private double score;
-
-    public Main(String name, int age, double score) {
-        setName(name);
-        setAge(age);
-        setScore(score);
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        if (age < 0) {
-            System.out.println("Invalid Age");
-        }else {
-            this.age = age;
-        }
-    }
-    public double getScore() {
-        return score;
-    }
-    public void setScore(double score) {
-        if (score < 0) {
-            System.out.println("Invalid Score");
-        }
-        else {
-            this.score = score;
-        }
-    }
-
 
     public static void main(String[] args) {
-        Main main = new Main("kaka",-1,-1);
-        System.out.println(main.getName() + " " + main.getAge() + " " + main.getScore());
+        ArrayList<Animal> list = new ArrayList<>();
+        list.add(new Dag("Dog", 12));
+        list.add(new Cat("Cat", 5));
+        for(Animal a : list){
+            a.Sound();
+        }
     }
 }

0580f190c698a3266ea523bf5627cb2576092885
LyMeng12 <mengsaoly@gmail.com>
2025-12-07 21:54:12 +0700

    OOP-encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,45 @@
 public class Main {
-    String name;
-    int age;
-    double score;
+    private String name;
+    private int age ;
+    private double score;
 
-    public Main(String name, int age , double score){
-        this.name = name;
-        this.age = age;
-        this.score = score;
+    public Main(String name, int age, double score) {
+        setName(name);
+        setAge(age);
+        setScore(score);
     }
-    public void display(){
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(score);
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if (age < 0) {
+            System.out.println("Invalid Age");
+        }else {
+            this.age = age;
+        }
+    }
+    public double getScore() {
+        return score;
+    }
+    public void setScore(double score) {
+        if (score < 0) {
+            System.out.println("Invalid Score");
+        }
+        else {
+            this.score = score;
+        }
     }
 
 
     public static void main(String[] args) {
-        Main main = new Main("John Smith", 20, 90);
-        main.display();
-
+        Main main = new Main("kaka",-1,-1);
+        System.out.println(main.getName() + " " + main.getAge() + " " + main.getScore());
     }
 }

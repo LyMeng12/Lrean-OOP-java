9c08655314090084bcc29a11528d0c48237ceb0a
LyMeng12 <mengsaoly@gmail.com>
2025-12-08 11:23:38 +0700

    HashSet input and display

--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashSet<String> set = new HashSet<>();
-        boolean add = true;
-        while (add){
-            System.out.println("How many words do you want to add?");
+        HashSet<Integer> inint = new HashSet<>();
+        HashSet<Integer> setin = new HashSet<>();
+        HashSet<Integer> setout = new HashSet<>();
+        int number =0;
+
+        for (int i = 0; i < 10; i++) {
             System.out.print("Enter number: ");
             int num = sc.nextInt();
-            while(num < 0){
-                System.out.print("Enter number again: ");
-                num = sc.nextInt();
+            if (setin.contains(num)) {
+                setout.add(num);
             }
-            System.out.println("Add auto :@gmail.com");
-            for (int i = 0; i < num; i++){
-                System.out.print("Enter Name Email:");
-               String name = sc.next();
-               String email = "@gmail.com";
-               set.add(name+email);
-            }
-            for(String s : set){
-                System.out.println(s);
-            }
-            System.out.print("you need more orr not(y/n):");
-            String answer = sc.next().toLowerCase();
-            while (!answer.equals("y") && !answer.equals("n")){
-                System.out.print("Choose again(y/n):");
-                answer = sc.next().toLowerCase();
-            }
-            if (answer.equals("y")){
-                add = true;
-            }else if (answer.equals("n")){
-                add = false;
-            }
+
+            setin.add(num);
         }
-        System.out.println("all done");
-        for(String s : set){
-            System.out.println(s);
+        setin.removeAll(setout);
+
+
+
+
+
+        for (Integer num : setout) {
+            System.out.print(num + " ");
         }
-
-
-
+        System.out.println();
+        for (Integer num : setin) {
+            System.out.print(num + " ");
+        }
     }
 }

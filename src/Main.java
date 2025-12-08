033aedaaaa77234f09d5eb91803a862ab4a25cfa
LyMeng12 <mengsaoly@gmail.com>
2025-12-08 09:40:56 +0700

    HashSet input Email and display.

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,43 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
-        System.out.println("How Many");
-        System.out.print("Enter number: ");
-        int num = sc.nextInt();
-
-        for (int i =0 ;i<num;i++) {
-            System.out.print((i+1)+"number:");
-            list.add(sc.nextInt());
+        HashSet<String> set = new HashSet<>();
+        boolean add = true;
+        while (add){
+            System.out.println("How many words do you want to add?");
+            System.out.print("Enter number: ");
+            int num = sc.nextInt();
+            while(num < 0){
+                System.out.print("Enter number again: ");
+                num = sc.nextInt();
+            }
+            System.out.println("Add auto :@gmail.com");
+            for (int i = 0; i < num; i++){
+                System.out.print("Enter Name Email:");
+               String name = sc.next();
+               String email = "@gmail.com";
+               set.add(name+email);
+            }
+            for(String s : set){
+                System.out.println(s);
+            }
+            System.out.print("you need more orr not(y/n):");
+            String answer = sc.next().toLowerCase();
+            while (!answer.equals("y") && !answer.equals("n")){
+                System.out.print("Choose again(y/n):");
+                answer = sc.next().toLowerCase();
+            }
+            if (answer.equals("y")){
+                add = true;
+            }else if (answer.equals("n")){
+                add = false;
+            }
         }
-        Collections.sort(list,Collections.reverseOrder());
-
-        for (int i =0 ;i<num;i++) {
-            System.out.print(list.get(i) + " ");
+        System.out.println("all done");
+        for(String s : set){
+            System.out.println(s);
         }
+
 
 
     }

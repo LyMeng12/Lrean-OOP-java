6585776a2e465f576e273e6bf8a5de000b0ea13f
LyMeng12 <mengsaoly@gmail.com>
2025-12-08 23:36:23 +0700

    LinkedListfind number have or haven't

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,19 +1,31 @@
+import java.util.ArrayList;
+
 public class LinkedList {
     public static void main(String[] args) {
-        java.util.LinkedList<String> list = new java.util.LinkedList<>();
+//        ArrayList<String> names = new ArrayList<>();
+        java.util.LinkedList<Integer> list = new java.util.LinkedList<>();
 
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        list.add("D");
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(5);
 
-        list.addLast("T");
-        list.addFirst("AA");
+        list.addLast(0);
+        list.addFirst(4);
 
-        list.removeLast();
-        list.removeFirst();
-        for(String s : list ){
-            System.out.print(s+" ");
+//        list.removeLast();
+//        list.removeFirst();
+        for (int i = 0; i < list.size(); i++) {
+            if(list.contains(i)) {
+                System.out.println("This Have");
+            }
+            else{
+                System.out.println("This not Have");
+            }
         }
+
+
+
+
     }
 }

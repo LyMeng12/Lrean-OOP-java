9e42ea3e575806448044bac2d623845010edff8e
LyMeng12 <mengsaoly@gmail.com>
2025-12-08 23:30:48 +0700

    LinkedList-tast Removelast and Removefirst

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -9,6 +9,9 @@ public class LinkedList {
 
         list.addLast("T");
         list.addFirst("AA");
+
+        list.removeLast();
+        list.removeFirst();
         for(String s : list ){
             System.out.print(s+" ");
         }

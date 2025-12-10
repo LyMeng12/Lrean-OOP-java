1bbd217e53ab5726cde13797c632d41b9aa7516a
LyMeng12 <mengsaoly@gmail.com>
2025-12-10 15:29:55 +0700

    HashMap already

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,25 @@
 import java.util.*;
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer,Integer> list = new HashMap<>();
+        HashMap<Integer,Integer> lists = new HashMap<>();
+        HashMap<Integer,Integer> test = new HashMap<>();
         boolean done = true;
         while (done) {
-            if (list.size() > 0) {
+            if (lists.size() > 0) {
                 System.out.println("=========== Menu ===========");
                 System.out.println("1.Add");
-                System.out.println("2.Insert");
-                System.out.println("3.Delete");
-                System.out.println("4.Update");
-                System.out.println("5.Display");
-                System.out.println("6.Sort");
-                System.out.println("7.Exit");
+                System.out.println("2.Delete");
+                System.out.println("3.Update");
+                System.out.println("4.Display");
+                System.out.println("5.Sort");
+                System.out.println("6.Exit");
                 System.out.print("Please Choose:");
                 int choose = sc.nextInt();
                 switch(choose){
@@ -24,71 +27,72 @@ public class Main {
                     case 1:{
                         System.out.print("Enter Number:");
                         int number = sc.nextInt();
-                        while (list.containsValue(number)){
+                        while (lists.containsValue(number)){
                             System.out.print("Enter Number again:");
                             number = sc.nextInt();
                         }
 
-                        list.put(list.size()+1,number);
-                    }break;
-//                    Insert
-                    case 2:{
-
+                        lists.put(lists.size()+1,number);
                     }break;
 //                    delete
-                    case 3:{
-                        for (Map.Entry<Integer,Integer> entry : list.entrySet()){
+                    case 2:{
+                        for (Map.Entry<Integer,Integer> entry : lists.entrySet()){
                             System.out.print(entry.getKey() + ":" + entry.getValue()+" ");
                         }
                         System.out.println();
 
                         System.out.println("Delete key number:");
                         int number = sc.nextInt();
-                        while (!list.containsValue(number)){
+                        while (!lists.containsValue(number)){
                             System.out.print("Delete Number again:");
                             number = sc.nextInt();
                         }
-                        list.remove(number);
+                        lists.remove(number);
                     }break;
 //                    update
-                    case 4:{
-                        for (Map.Entry<Integer,Integer> entry : list.entrySet()){
+                    case 3:{
+                        for (Map.Entry<Integer,Integer> entry : lists.entrySet()){
                             System.out.print(entry.getKey() + ":" + entry.getValue()+" ");
                         }
                         System.out.println();
                         System.out.print("Enter Number Key:");
                         int key = sc.nextInt();
-                        while (!list.containsKey(key)){
+                        while (!lists.containsKey(key)){
                             System.out.print("Enter Number again:");
                             key = sc.nextInt();
                         }
 
                         System.out.print("Enter Number:");
                         int number = sc.nextInt();
-                        while (list.containsValue(number)){
+                        while (lists.containsValue(number)){
                             System.out.print("Enter Number again:");
                             number = sc.nextInt();
                         }
-                        list.put(key,number);
+                        lists.put(key,number);
 
                     }break;
 //                    display
-                    case 5:{
-                        for(Map.Entry<Integer,Integer> entry : list.entrySet()){
-                            System.out.println(entry.getKey()+":"+entry.getValue());
+                    case 4:{
+                        for(Map.Entry<Integer,Integer> entry : lists.entrySet()){
+                            System.out.println(entry.getValue());
                         }
                     }break;
 //                    sort
-                    case 6:{
-
+                    case 5:{
+                        List<Map.Entry<Integer, Integer>> list =
+                                new ArrayList<>(lists.entrySet());
+                        Collections.sort(list, (a, b) -> b.getValue() - a.getValue());
+                        for (Map.Entry<Integer,Integer> entry : list){
+                            System.out.println(entry.getValue());
+                        }
                     }break;
 
-                    case 7:{
+                    case 6:{
                         done=false;
                     }break;
 
                 }
-            }else if (list.size() == 0){
+            }else if (lists.size() == 0){
                 System.out.println("1.add");
                 System.out.println("2.Exit");
                 System.out.print("Please Choose:");
@@ -96,7 +100,7 @@ public class Main {
                 if (choose == 1){
 
                     System.out.print("Number:");
-                    list.put(list.size()+1,sc.nextInt());
+                    lists.put(lists.size()+1,sc.nextInt());
 
                 }else if (choose == 2){
                     done=false;

5723e4ea4d897ba597f68e036d198d02fb17d8e2
LyMeng12 <mengsaoly@gmail.com>
2026-03-10 21:40:15 +0700

    3. Multiple Type Parameters

--- src/Java_Advanced/Box.java
+++ src/Java_Advanced/Box.java
@@ -1,11 +1,16 @@
 package Java_Advanced;
 
-public class Box <T>{
-    private T data;
-    public void setData(T data){
-        this.data = data;
+public class Box <T,U>{
+    private T t;
+    private U u;
+    public Box(T t, U u){
+        this.t = t;
+        this.u = u;
     }
-    public T getData(){
-        return this.data;
+    public T getT() {
+        return t;
     }
+   public U getU() {
+        return u;
+   }
 }
--- src/Java_Advanced/Generics.java
+++ src/Java_Advanced/Generics.java
@@ -1,37 +1,19 @@
 package Java_Advanced;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generics {
-
-
-
-//    Generic Method
-    public static <T> void printData(T data) {
-        System.out.println(data);
-    }
-
 
     public static void main(String[] args) {
 
 //        Generics = គឺជាវិធីសាស្ត្រដែលអនុញ្ញាតឲ្យយើងសរសេរ class, interface, និង method ដែលអាចធ្វើការជាមួយ data type ផ្សេងៗបាន
 //                     ដោយមិនចាំបាច់សរសេរកូដដដែលៗ។
 
+//        Multiple Type Parameters<T,U>
+//                |
+        Box<String,Integer> nsj = new Box<>("meng",120);
+        System.out.println(nsj.getT()+" "+nsj.getU());
 
-//       2. Generic Method
-
-//       <T>=type parameter
-//              |
-        Box<String> box = new Box<>();
-        box.setData("Hello");
-        System.out.println(box.getData());  //Hello
-
-//       <T>=type parameter
-//              |
-        Box<Integer> box2 = new Box<>();
-        box2.setData(123);
-        System.out.println(box2.getData()); //123
-
-        printData("Java");
-        printData(100);
-        printData(5.5);
     }
 }
